02b6599c619549d5e0434c039de94fd3ab6fcdc4
ftm1337 <ftm1337@protonmail.com>
2025-10-23 22:53:32 -0400

    update eliteness bridge

---
+++ docs/elite.js
@@ -0,0 +1,37 @@
+// static data
+
+    // Main Meta
+    TREASURY_TOKEN_TICKER = "ELITE";
+    TREASURY_TOKEN_NAME = "Eliteness.Network";
+    TREASURY_TOKEN_LONGNAME = "Eliteness Network";
+
+	// Special Notes, separated by backticks
+	TREASURY_NOTES = [
+		`Track Assets Under Management on <a target="_blank" href="https://debank.com/bundles/461">DeBank's Bundle 461</a>, including proof-of-reserves for public sub-protocols.` ,
+		`ELITE price before March 2025 has been adjusted to 1/300000 to reflect the 1:300K <a target="_blank" href="https://medium.com/@ftm1337/elite-eliteness-network-31086677e2c2">migration from old ELITE</a> on Fantom to new omnichain ELITE.` ,
+		`Token Supply number represents only issued the supply not held by Treasury. It is not Circulating supply, neither Total Supply or Unlocked supply, but only the Supply that is externally owned by anyone else, excluding the Treasury itself.` ,
+	]
+
+	// Address list
+	TREASURIES_LIST= [
+		["0x0402af87a3024a5c767b47f0ad42d887014e1d01",""],
+		["0x3ae524d3d330130ab22c3719ca913a3d4a8de504",""],
+		["0x4496323dd41417634b8412091a0b821c36922408",""]
+	]
+
+	// [ wen, marketPrice, classA, classB, classC, supply ]
+	TADATA = [
+		[ "2022-04-03",	 2503/300_000,       75_000,             0,              0,		400_000_000 ],
+		[ "2022-08-18",	 2619/300_000,       30_000,        24_000,              0,		400_000_000 ],
+		[ "2022-11-05",	 2086/300_000,       24_000,        19_000,         98_500,		400_000_000 ],
+		[ "2023-01-09",	  948/300_000,       57_000,             0,        270_000,		400_000_000 ],
+		[ "2023-06-29",	 1019/300_000,      160_000,        85_000,        245_000,		400_000_000 ],
+		[ "2023-10-09",	  784/300_000,      102_500,        51_000,        235_000,		400_000_000 ],		// count 800k ELR @ $0.015
+		[ "2024-03-19",	 2350/300_000,      336_000,       215_000,      1_234_000,		400_000_000 ],
+		[ "2024-05-26",	 3692/300_000,      640_000,     1_075_000,        255_000,		400_000_000 ],
+		[ "2024-08-05",	 1174/300_000,	    970_000,       302_000,        101_000,		400_000_000 ],
+		[ "2024-11-07",	 2543/300_000,	  1_074_000,       529_000,        107_000,		400_000_000 ],
+		[ "2025-02-28",	  957/300_000,	    900_000,       100_000,         50_000,		400_000_000 ],
+		[ "2025-04-05",	     0.002647,	    769_000,       300_000,         33_000,		400_000_000 ],
+		[ "2025-07-23",	     0.002357,	  1_120_000,       121_000,         65_000,		400_000_000 ],
+	]
